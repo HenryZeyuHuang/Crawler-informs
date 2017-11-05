6d7559227c799fc9a08dffcb7ae8d18a6bc71938
HenryZeyuHuang <huangzeyu1007@gmail.com>
2017-11-05 16:52:11 -0500

    Changed UI instructions

--- Start.py
+++ Start.py
@@ -2,12 +2,13 @@ from Crawler import find_info
 from Links import find_internal_links, generate_link_dataframe
 
 control = "continue"
+info = "Choose command from following list\n"\
+       "-- info : Generate a spreadsheet of personal information Approx. 15 min\n" \
+       "-- find : Find all the pages under informs. Approx. 10 min\n" \
+       "-- link : Output dead links after 'find' is excuted. Approx. 4 hrs\n" \
+       "-- exit : Exit the program\n"
 
-print("Choose command from following list\n"
-          "-- info : Generate a spreadsheet of personal information Approx. 15 min\n"
-          "-- find : Find all the pages under informs. Approx. 10 min\n"
-          "-- link : Output dead links after 'find' is excuted. Approx. 4 hrs\n"
-          "-- exit : Exit the program\n")
+print(info)
 
 while True:
 
@@ -26,8 +27,4 @@ while True:
     else:
         print("Command not recognized\n")
 
-    print("Choose command from following list\n"
-          "-- info : Generate a spreadsheet of personal information Approx. 15 min\n"
-          "-- find : Find all the pages under informs. Approx. 10 min\n"
-          "-- link : Output dead links after 'find' is excuted. Approx. min\n"
-          "-- exit : Exit the program\n")
+    print(info)
